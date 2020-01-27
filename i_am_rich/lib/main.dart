a10a919dcb80ac55a356c48577b4196334ff4ca7
jun108059 <jun108059@hufs.ac.kr>
2020-01-27 11:24:54 +0900

    [Update] Scaffold body + image

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 // The main function is the starting point for all out Flutter apps.
 void main() => runApp(
       MaterialApp(
-        home: Center(
-          child: Text('Hello World'),
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text('I Am Rich'),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+              image: NetworkImage('https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg'),
+            ),
+          ),
         ),
       ),
     );
