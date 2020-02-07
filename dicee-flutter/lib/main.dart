23bda9f88ad5c4a93551365fc856693eaeffc7ef
jun108059 <youngjun108059@gmail.com>
2020-02-07 18:53:46 +0900

    [Update] Change Stateless to StateFul

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -15,7 +15,14 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1; // 매번 새로 생성할 필요 없기 때문에
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -23,17 +30,20 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice1.png'),
               onPressed: () {
-                print('Left buffon got pressed.');
+                setState(() { // HotReload 없이 Image 바뀜 - Trigger
+                  leftDiceNumber = 3;
+                  print('deceNumber = $leftDiceNumber');
+                });
               },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             child: FlatButton(
               child: Image.asset('images/dice2.png'),
               onPressed: () {
-
+                print('Right button got pressed.');
               },
             ),
           ),
