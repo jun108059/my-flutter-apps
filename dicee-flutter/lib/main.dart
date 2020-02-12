470710047c0d4d1ea760bd35a164d50fed2f9ca4
jun108059 <youngjun108059@gmail.com>
2020-02-12 10:11:58 +0900

    [Update] Make chaceDiceFace function

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -25,6 +25,14 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 5; // 매번 새로 생성할 필요 없기 때문에 한번만 초기화
   int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() { // HotReload 없이 Image 바뀜 - Trigger
+      leftDiceNumber = Random().nextInt(6) + 1; // Random Number
+      rightDiceNumber = Random().nextInt(6) + 1; // Random Number
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -33,9 +41,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                setState(() { // HotReload 없이 Image 바뀜 - Trigger
-                  leftDiceNumber = Random().nextInt(6) + 1; // Random Number
-                });
+                changeDiceFace();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -44,10 +50,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                setState(() { // HotReload 없이 Image 바뀜 - Trigger
-                  leftDiceNumber = Random().nextInt(6) + 1; // Random Number
-                  rightDiceNumber = Random().nextInt(6) + 1; // Random Number
-                });
+                changeDiceFace();
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
