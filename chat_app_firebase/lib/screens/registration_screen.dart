7a25e2bae4e357a1eba32769b99385346224b9e1
jun108059 <youngjun108059@gmail.com>
2020-02-26 17:01:56 +0900

    [Update] async and check - try, catch

--- chat_app_firebase/lib/screens/registration_screen.dart
+++ chat_app_firebase/lib/screens/registration_screen.dart
@@ -1,7 +1,9 @@
 import 'package:chat_app_firebase/constants.dart';
+import 'package:chat_app_firebase/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_app_firebase/components/rounded_button.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'chat_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'registration_screen';
@@ -11,6 +13,7 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
   String email;
   String password;
 
@@ -35,19 +38,25 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 48.0,
             ),
             TextField(
+              keyboardType: TextInputType.emailAddress,  // 이메일 형식
+              textAlign: TextAlign.center,
               onChanged: (value) {
                 email = value;
               },
-              decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your email'),
+              decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your email'),
             ),
             SizedBox(
               height: 8.0,
             ),
             TextField(
+              obscureText: true,  // 비밀번호로 표시
+              textAlign: TextAlign.center,  // 가운데 정렬
               onChanged: (value) {
                 password = value;
               },
-              decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your password'),
+              decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your password'),
             ),
             SizedBox(
               height: 24.0,
@@ -55,9 +64,17 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             RoundedButton(
               title: 'Register',
               colour: Colors.blueAccent,
-              onPressed:() {
-                print(email);
-                print(password);
+              onPressed:() async {
+                try{  // 유효하지 않은 값이 들어왔을 때 처리하는 try catch
+                  final newUser = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+                  if (newUser != null) {
+                    Navigator.pushNamed(context, ChatScreen.id);
+                  }
+                }
+                catch (e) {
+                  print(e);
+                }
+
               },
             ),
           ],
