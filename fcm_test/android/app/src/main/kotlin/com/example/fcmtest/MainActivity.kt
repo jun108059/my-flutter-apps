5dc1b34bfe46a3c0c6acb1b532a01650df5ee0e1
jun108059 <youngjun108059@gmail.com>
2020-04-09 20:37:51 +0900

    [Create] fcm_test Flutter project

---
+++ fcm_test/android/app/src/main/kotlin/com/example/fcmtest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fcmtest
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
